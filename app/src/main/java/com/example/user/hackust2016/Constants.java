dde01e8329a49476f6d41feba298ef82c9820ff4
ken93939 <ken93939@gmail.com>
2016-04-16 16:47:39 +0800

    123321

---
+++ app/src/main/java/com/example/user/hackust2016/Constants.java
@@ -0,0 +1,9 @@
+package com.example.user.hackust2016;
+
+/**
+ * Created by User on 4/16/2016.
+ */
+public class Constants {
+    public static final String serverIp="10.89.126.121:3000/api";
+    public static final String idk="das";
+}
--- app/src/main/java/com/example/user/hackust2016/Welcome.java
+++ app/src/main/java/com/example/user/hackust2016/Welcome.java
@@ -2,12 +2,25 @@ package com.example.user.hackust2016;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 
 public class Welcome extends AppCompatActivity {
+    private GestureDetector mDetector;
+    private int page;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        page=0;
+
     }
+    private class swipeWelcome extends GestureDetector.SimpleOnGestureListener{
+        @Override
+        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+            return super.onFling(e1, e2, velocityX, velocityY);
+        }
+    }
+
 }
